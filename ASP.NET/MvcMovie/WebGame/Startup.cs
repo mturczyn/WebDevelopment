176d69acfafc65cad7969a608711ec6239ccec50
Michal <turek1992@o2.pl>
2020-03-31 13:01:10 +0200

    Uzupelnienie notatek do buga

--- ASP.NET/MvcMovie/WebGame/Startup.cs
+++ ASP.NET/MvcMovie/WebGame/Startup.cs
@@ -31,7 +31,7 @@ namespace WebGame
       services.Configure<CookiePolicyOptions>(options =>
       {
         // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-        options.CheckConsentNeeded = context => false;
+        options.CheckConsentNeeded = context => true;
         options.MinimumSameSitePolicy = SameSiteMode.None;
       });
       services.Configure<MvcOptions>(op => op.EnableEndpointRouting = false);
